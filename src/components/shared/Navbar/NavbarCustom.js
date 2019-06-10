2ea11337206cbe25565568cc8b338610dd99bef5
cholical <ronding@live.unc.edu>
2019-06-10 12:49:43 -0400

    Navbar working as desired except for colors

--- src/components/shared/Navbar/NavbarCustom.js
+++ src/components/shared/Navbar/NavbarCustom.js
@@ -9,42 +9,46 @@ class NavbarCustom extends React.Component {
 		super(props);
 
 		this.state = {
-			collapsed: true,
-			navbarBg: '',
-			navbarCollapseCSS: ''
+			expanded: false,
+			expandedColor: false,
 		}
 		this.test = this.test.bind(this);
-		this.toggleCollapse = this.toggleCollapse.bind(this);
+		this.toggleNavbar = this.toggleNavbar.bind(this);
 
 		this.navbarCollapseRef = React.createRef();
 	}
 
 	test() {
-		console.log(this.state.navbarCollapseCSS);
-		this.setState({navbarCollapseCSS: ''});
+		this.setState({expanded: false});
+		setTimeout(function() {
+			this.setState({expandedColor: false});
+		}.bind(this), 250);
 	}
 
-	toggleCollapse() {
-		var collapsedState = this.state.collapsed;
-		if (collapsedState) {
-			this.setState({collapsed: !collapsedState});
-		} else {
+	toggleNavbar() {
+		const expandedState = this.state.expanded;
+		const expandedColor = this.state.expandedColor;
+		if (expandedState) {
+			this.setState({expanded: !expandedState});
 			setTimeout(function() {
-				this.setState({collapsed: !collapsedState});
+				this.setState({expandedColor: !expandedColor});
 			}.bind(this), 250);
+		} else {
+			this.setState({expanded: !expandedState});
+			this.setState({expandedColor: !expandedColor});
 		}
 	}
 
 	render() {
 		return (
-			<Navbar expand="md" bg={this.state.collapsed ? "" : "dark"} variant="dark" collapseOnSelect="true" fixed="top">
-				<Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.toggleCollapse}/>
-				<Navbar.Collapse id="basic-navbar-nav" bsPrefix={this.state.navbarCollapseCSS}>
+			<Navbar expand="md" bg={this.state.expandedColor ? "dark" : ""} variant="dark" collapseOnSelect="true" fixed="top" onToggle={this.toggleNavbar} expanded={this.state.expanded}>
+				<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav>
-						<Nav.Link onSelect={this.test} as={Link} to="/" onClick={this.test}>Home</Nav.Link>
-						<Nav.Link as={Link} to="/projects/" >Projects</Nav.Link>
-						<Nav.Link as={Link} to="/about/">About</Nav.Link>
-						<Nav.Link as={Link} to="/contact/">Contact</Nav.Link>
+						<Nav.Link as={Link} to="/" onClick={this.test}>Home</Nav.Link>
+						<Nav.Link as={Link} to="/projects/" onClick={this.test}>Projects</Nav.Link>
+						<Nav.Link as={Link} to="/about/" onClick={this.test}>About</Nav.Link>
+						<Nav.Link as={Link} to="/contact/" onClick={this.test}>Contact</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>

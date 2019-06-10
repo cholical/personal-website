b7c49eb1112f928e92d6aa29a453698bdc97a631
cholical <ronding@live.unc.edu>
2019-06-10 12:22:31 -0400

    Enhancements to the navbar

--- src/components/shared/Navbar/NavbarCustom.js
+++ src/components/shared/Navbar/NavbarCustom.js
@@ -7,20 +7,41 @@ class NavbarCustom extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			collapsed: true,
+			navbarBg: '',
+			navbarCollapseCSS: ''
+		}
 		this.test = this.test.bind(this);
+		this.toggleCollapse = this.toggleCollapse.bind(this);
+
+		this.navbarCollapseRef = React.createRef();
 	}
 
 	test() {
-		console.log('test');
+		console.log(this.state.navbarCollapseCSS);
+		this.setState({navbarCollapseCSS: ''});
+	}
+
+	toggleCollapse() {
+		var collapsedState = this.state.collapsed;
+		if (collapsedState) {
+			this.setState({collapsed: !collapsedState});
+		} else {
+			setTimeout(function() {
+				this.setState({collapsed: !collapsedState});
+			}.bind(this), 250);
+		}
 	}
 
 	render() {
 		return (
-			<Navbar expand="md" variant="dark" collapseOnSelect="true" fixed="top">
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
+			<Navbar expand="md" bg={this.state.collapsed ? "" : "dark"} variant="dark" collapseOnSelect="true" fixed="top">
+				<Navbar.Toggle aria-controls="basic-navbar-nav" onClick={this.toggleCollapse}/>
+				<Navbar.Collapse id="basic-navbar-nav" bsPrefix={this.state.navbarCollapseCSS}>
 					<Nav>
-						<Nav.Link onSelect={this.test} as={Link} to="/">Home</Nav.Link>
+						<Nav.Link onSelect={this.test} as={Link} to="/" onClick={this.test}>Home</Nav.Link>
 						<Nav.Link as={Link} to="/projects/" >Projects</Nav.Link>
 						<Nav.Link as={Link} to="/about/">About</Nav.Link>
 						<Nav.Link as={Link} to="/contact/">Contact</Nav.Link>
